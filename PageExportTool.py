966462bb750fd98eec541d92ef3aff120d64c901
Michael D'Ambrogia <dambrogia@gmail.com>
2018-11-10 16:46:31 +0700

    Update PageExportTool.py

--- PageExportTool.py
+++ PageExportTool.py
@@ -1,19 +1,24 @@
+# import the following Python modules for needed functionality
 import requests
 import json
 import csv
 
-
-
+# set the base URL to the eCX API, and the endpoint
 base_url = "https://api.sandbox.ecrimex.net"
 phish_endpoint = "/phish"
+query_params = ""
 
+# the request will need to have headers set, the Authorization value should be set to your eCX API token key which 
+# you can find in your eCX profile at https://ecrimex.net/users/update
 headers = {
     'Authorization': "",
     'Content-Type': "application/json"
     }
 
+# function to call the eCX API
 def extractPhishData(endpoint):
     url = base_url + endpoint
+    # add the query_params variable to the request if you want to filter your data or set the output format
     response = requests.request("GET", url, headers=headers)
     data = json.dumps(response.json())
     api_data = json.loads(data)
@@ -22,22 +27,26 @@ def extractPhishData(endpoint):
     return_data = {'phish': api_phish, 'links': links}
     return return_data
 
+# our array to hold the output data
 output_data = []
 
+#call our function to GET the data
 temp_data = extractPhishData(phish_endpoint)
 
+# in the results of the output is a _links JSON array, save the array into links
 links = temp_data['links']
 
 output_data.extend(temp_data['phish'])
 
+# iterate the results as long as links has a `next` key in the array
 while 'next' in links:
     next_url = links['next']['href']
     new_phish_data = extractPhishData(next_url)
     links = new_phish_data['links']
     output_data.extend(new_phish_data['phish'])
 
-# Below is how you export data into a CSV file.
-
+# manually convert the data and output it into a CSV file.
+# alternately you could add 'container=csv' to query_params and include it in the request above
 with open('Output_Phish_Data.csv', 'w',newline='') as new_csv_file:
     fieldnames = ['_links','id','url','brand','confidence_level','date_discovered','modified','asn','ip','domain','metadata','status']
     csv_writer = csv.DictWriter(new_csv_file, fieldnames=fieldnames, delimiter= ',')
